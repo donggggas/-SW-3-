e53e5bf0eaa7c30abb3fb5000d712938e37ec604
kim-dong-woo <dongwoo12048@gmail.com>
2020-05-07 02:16:07 +0900

    6th

---
+++ 6th/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://kim-dong-woo.github.io");
+    }
+}
